4b156f3b9bb6524b4c8501e0f016205d8fa1078a
Jing <jingbeta@gmail.com>
2010-10-04 01:04:00 -0500

    adding TCP socket communication protocol between Server and Client

---
+++ Sockets/Client/DotNetSocClient.NET/tcpClient.cs
@@ -0,0 +1,166 @@
+// tcpClient.java by fpont 3/2000
+
+// usage : java tcpClient <server> <port>
+// default port is 1500
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+public class StateObject 
+{
+	// Client socket.
+	public Socket workSocket = null;
+	// Size of receive buffer.
+	public const int BufferSize = 256;
+	// Receive buffer.
+	public byte[] buffer = new byte[BufferSize];
+	// Received data string.
+	public StringBuilder sb = new StringBuilder();
+}
+
+public class tcpClient
+{
+	static ManualResetEvent receiveDone = new ManualResetEvent(false);
+    static System.Net.Sockets.Socket socket = null;
+    static System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+
+	[STAThread]
+	public static void  Main(System.String[] args)
+	{
+		int port = 1500;
+		System.String server = "nt126101";
+		
+		System.String lineToBeSent;
+		System.IO.StreamReader input;
+		System.IO.StreamWriter output;
+		int ERROR = 1;
+
+        System.Console.Out.WriteLine("server port = 1500 (default)");
+        port = 1500;
+		
+		try
+		{
+			//UPGRADE_TODO: Expected value of parameters of constructor 'java.io.BufferedReader.BufferedReader' are different in the equivalent in .NET. 'ms-help://MS.VSCC.2003/commoner/redir/redirect.htm?keyword="jlca1092"'
+			//UPGRADE_ISSUE: 'java.lang.System.in' was converted to 'System.Console.In' which is not valid in this expression. 'ms-help://MS.VSCC.2003/commoner/redir/redirect.htm?keyword="jlca1109"'
+			//			input = new System.IO.StreamReader(new System.IO.StreamReader(System.Console.In).BaseStream, System.Text.Encoding.UTF7);
+			//			System.IO.StreamWriter temp_writer;
+			//			temp_writer = new System.IO.StreamWriter((System.IO.Stream) socket.GetStream());
+			//			temp_writer.AutoFlush = true;
+			//			output = temp_writer;
+			
+			// get user input and transmit it to server
+
+            socket = new System.Net.Sockets.Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+			System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse("192.168.1.12"); 
+			System.Net.IPEndPoint remoteEP = new System.Net.IPEndPoint(ipAdd,1500);
+
+			socket.Connect(remoteEP);
+			//Async Read form the server side
+			Receive(socket);
+			//new System.Net.Sockets.TcpClient(server, port);
+			while(true)
+			{
+				lineToBeSent = System.Console.ReadLine();
+				
+				// stop if input line is "."
+				if(lineToBeSent.Equals("."))
+				{
+					socket.Close();
+					break;
+				}
+                lineToBeSent += "\n";
+				//output.WriteLine(lineToBeSent);\
+				//System.Net.Sockets.NetworkStream tempstream = socket.GetStream();
+                tcpSend(socket, lineToBeSent, encoding);
+			}
+			//socket.Close();
+
+			
+			byte[] Serbyte = new byte[30];
+			//			socket.Receive(Serbyte,0,20,System.Net.Sockets.SocketFlags.None);
+			//			System.Console.WriteLine("Message from server \n" + encoding.GetString(Serbyte));
+
+			//socket.Close();
+		
+		
+		}
+		catch (System.IO.IOException e)
+		{
+			System.Console.Out.WriteLine(e);
+		}		
+	}
+
+   
+
+    private void tcpSend(System.String lineToBeSent)
+    {
+        this.socket.Send(encoding.GetBytes(lineToBeSent));
+    }
+
+	private static void Receive(Socket client) 
+	{
+		try 
+		{
+			// Create the state object.
+			StateObject state = new StateObject();
+			state.workSocket = client;
+
+			// Begin receiving the data from the remote device.
+			client.BeginReceive( state.buffer, 0, StateObject.BufferSize, 0,
+				new AsyncCallback(ReceiveCallback), state);
+			
+		} 
+		catch (Exception e) 
+		{
+			Console.WriteLine(e.ToString());
+		}
+	}
+
+	static string response = "";
+	private static void ReceiveCallback( IAsyncResult ar ) 
+	{
+		try 
+		{
+			// Retrieve the state object and the client socket 
+			// from the asynchronous state object.
+		
+			StateObject state = (StateObject) ar.AsyncState;
+			Socket client = state.workSocket;
+			// Read data from the remote device.
+			int bytesRead = client.EndReceive(ar);
+			if (bytesRead > 0) 
+			{
+				// There might be more data, so store the data received so far.
+				state.sb.Append(Encoding.ASCII.GetString(state.buffer,0,bytesRead));
+				//  Get the rest of the data.
+				if (state.sb.Length > 1) 
+				{
+					response = Encoding.ASCII.GetString(state.buffer,0,bytesRead).ToString();
+                    response = response.Substring(0, response.IndexOf("\n")+1);
+					Console.WriteLine(response);
+				}
+				client.BeginReceive(state.buffer,0,StateObject.BufferSize,0,
+					new AsyncCallback(ReceiveCallback), state);
+			} 
+			else 
+			{
+				// All the data has arrived; put it in response.
+				if (state.sb.Length > 1) 
+				{
+					response = state.sb.ToString();
+					Console.WriteLine(response);
+				}
+				// Signal that all bytes have been received.
+				
+				//receiveDone.Set();
+				
+			}
+		} 
+		catch (Exception e) 
+		{
+			Console.WriteLine(e.ToString());
+		}
+	}
+
+
+}
---
+++ Sockets/Client/DotNetSocClient.NET/tcpServer.java
@@ -0,0 +1,98 @@
+// tcpServer.java by fpont 3/2000
+
+// usage : java tcpServer <port number>.
+// default port is 1500.
+// connection to be closed by client.
+// this server handles only 1 connection.
+
+import java.net.*;
+import java.io.*;
+import java.lang.*;
+
+public class tcpServer {
+    
+    public static void main(String args[]) {
+	
+	int port;
+	ServerSocket server_socket;
+	BufferedReader input;
+	BufferedWriter output;
+	try { 
+	    port = Integer.parseInt(args[0]);
+	}
+	catch (Exception e) {
+	    System.out.println("port = 1500 (default)");
+	    port = 1500;
+	}
+
+	try {
+	    
+	    server_socket = new ServerSocket(port);
+	    System.out.println("Server waiting for client on port " +
+			       server_socket.getLocalPort());
+	    
+	    // server infinite loop
+	    while(true) {
+		Socket socket = server_socket.accept();
+		System.out.println("New connection accepted " +
+				   socket.getInetAddress() +
+				   ":" + socket.getPort());
+
+
+    input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    output = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		// print received data
+		try {
+
+		    while(true) {
+      char[] buf = new char[20];
+
+       input.read(buf,0,20);
+       if (buf==null) {
+       return;
+       }
+        System.out.println(buf);
+      // input.close();
+
+
+       // output.flush();
+//        for(int i=0;i<15;i++)
+        {
+        //Flushing the output after the write will immediatly intimate the client
+            output.write(buf,0,20);
+            output.flush();
+        }
+          //output.write(buf);
+        //output.flush();
+
+
+		    }
+		}
+		catch (IOException e) {
+		    System.out.println(e);
+		}
+
+
+		// connection closed by client
+		try {
+		    socket.close();
+		    System.out.println("Connection closed by client");
+		}
+		catch (IOException e) {
+		    System.out.println(e);
+		}
+		
+	    }
+	    
+	    
+	}
+	
+	catch (IOException e) {
+	    System.out.println(e);
+	}
+    }
+}
+		
+
+
+		
